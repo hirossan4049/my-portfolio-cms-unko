acd8197c84c8d45abd2d75e30957cc241ede00bb
Mx244xt <dev.se.masuda@gmail.com>
2024-04-12 21:58:28 +0900

    add: api

---
+++ src/api/apiUrl.ts
@@ -0,0 +1,6 @@
+const apiUrl = {
+  PUT_TOP_DATA_URL: "",
+  CHECK_AUTH_URL: "",
+}
+
+export default apiUrl
--- src/api/useApi.ts
+++ src/api/useApi.ts
@@ -1,11 +1,49 @@
 import apiUrl from "./apiUrl";
 
+type PutTopImage = {
+  uid: string;
+  image?: File[];
+  productText?: string | null;
+  learningText?: string | null;
+  profileText?: string | null;
+  contactText?: string | null;
+};
+
 type LoginType = {
   loginId: string;
   password: string;
 };
 
-const { CHECK_AUTH_URL } = apiUrl;
+const { PUT_TOP_DATA_URL, CHECK_AUTH_URL } = apiUrl;
+
+export const putTopData = async ({ uid, image, productText, learningText, profileText, contactText }: PutTopImage) => {
+  try {
+    const data = {
+      uid: uid,
+      image: image,
+      productText: productText,
+      learningText: learningText,
+      profileText: profileText,
+      contactText: contactText,
+    };
+
+    const headers = {
+      'Content-type': 'Application/json',
+    };
+
+    const options = {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data),
+    };
+
+    const response: Response = await fetch(PUT_TOP_DATA_URL, options);
+    return response.json();
+  } catch (error) {
+    console.error(error)
+    return error;
+  }
+};
 
 export const checkAuth = async ({ loginId, password }: LoginType) => {
   try {
